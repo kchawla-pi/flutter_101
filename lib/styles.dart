da9d0dde19b0da74678a28b9065bbc428ee16f08
Kshitij Chawla (kchawla-pi) <kc.insight.pi@gmail.com>
2023-03-05 16:42:26 -0500

    Added: Custom font asset & used in styling

--- lib/styles.dart
+++ lib/styles.dart
@@ -7,15 +7,20 @@ Color hex2Color(String code) {
 class Styles {
   static const _textSizeLarge = 25.0;
   static const _textSizeDefault = 16.0;
+  static const _fontNameDefault = "CascadiaCode";
+  static final Color _textColorStrong = hex2Color('000000');
+  static final Color _textColorDefault = hex2Color('666666');
 
-  static final _textColorStrong = hex2Color('000000');
-  static final _textColorDefault = hex2Color('666666');
-
+  static const navBarTitle = TextStyle(
+    fontFamily: _fontNameDefault,
+  );
   static final headerLarge = TextStyle(
+    fontFamily: _fontNameDefault,
     fontSize: _textSizeLarge,
     color: _textColorStrong,
   );
   static final textDefault = TextStyle(
+    fontFamily: _fontNameDefault,
     fontSize: _textSizeDefault,
     color: _textColorDefault,
   );
